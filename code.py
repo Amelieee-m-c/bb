0019e31cec84aff1471ba538cb6a1767d8ebd8ea
Tricia <tinajay94@gmail.com>
2024-12-26 20:51:45 +0800

    first commit

---
+++ code.py
@@ -0,0 +1,42 @@
+[33mcommit 5a3fdae7aa84b8547f72a7125c906f121b6068ed[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m)[m
+Author: Tricia <tinajay94@gmail.com>
+Date:   Thu Dec 26 15:32:43 2024 +0800
+
+    change file name
+
+[1mdiff --git a/code.py b/minus.py[m
+[1msimilarity index 100%[m
+[1mrename from code.py[m
+[1mrename to minus.py[m
+
+[33mcommit 94a8ae1ae13f8ebf4b4353b2159beadfae88a3ef[m
+Author: Tricia <tinajay94@gmail.com>
+Date:   Thu Dec 26 15:15:46 2024 +0800
+
+    change addition to minus
+
+[1mdiff --git a/code.py b/code.py[m
+[1mindex dffa4c7..c8a9dae 100644[m
+[1m--- a/code.py[m
+[1m+++ b/code.py[m
+[36m@@ -1,3 +1,3 @@[m
+ a = int(input("enter a : "))[m
+ b = int(input("enter b : "))[m
+[31m-print(a+b)[m
+[32m+[m[32mprint(a-b)[m
+
+[33mcommit f6e821dea1c04a75b805b5c4bd70e23a6a2a53c6[m
+Author: Tricia <tinajay94@gmail.com>
+Date:   Thu Dec 26 15:05:34 2024 +0800
+
+    first commit
+
+[1mdiff --git a/code.py b/code.py[m
+[1mnew file mode 100644[m
+[1mindex 0000000..dffa4c7[m
+[1m--- /dev/null[m
+[1m+++ b/code.py[m
+[36m@@ -0,0 +1,3 @@[m
+[32m+[m[32ma = int(input("enter a : "))[m
+[32m+[m[32mb = int(input("enter b : "))[m
+[32m+[m[32mprint(a+b)[m
